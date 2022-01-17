3c8057f013511a49da0cf1432e0b424ed0ae3894
Maksym Binkovskyi <mbinkovskiy@gmail.com>
2022-01-17 15:24:31 +0200

    use kotlin build

---
+++ build.gradle.kts
@@ -0,0 +1,76 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+
+plugins {
+    id("ru.vyarus.quality").version("4.7.0")
+    id("java-library")
+    id("jacoco")
+    id("maven-publish")
+}
+
+val suitesDir = "src/test/resources/suites/"
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://jitpack.io") }
+}
+
+dependencies {
+    implementation("org.testng", "testng", "7.3.0")
+    implementation("joda-time", "joda-time", "2.10.9")
+    implementation("com.fasterxml.jackson.core", "jackson-databind", "2.11.0")
+}
+
+tasks.test {
+    useTestNG {
+        // Add test suites
+        File(projectDir.absolutePath + "/" + suitesDir)
+                .walk()
+                .forEach {
+                    if (it.isFile) {
+                        suites(it)
+                    }
+                }
+
+        testLogging {
+            events("passed", "skipped", "failed")
+            exceptionFormat = TestExceptionFormat.FULL
+            showStandardStreams = true
+        }
+    }
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+        html.outputLocation.set(layout.buildDirectory.dir("${buildDir}/reports/coverage").get().asFile)
+    }
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+quality {
+    checkstyle = true
+    pmd = true
+    codenarc = true
+    spotbugs = true
+}
+
+tasks.check {
+    dependsOn(tasks.jacocoTestReport)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.mbi"
+            artifactId = "date-handler"
+            version = "1.0"
+
+            from(components["java"])
+        }
+    }
+}
